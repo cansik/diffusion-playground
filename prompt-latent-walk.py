c6035c8cce6eac46bdea4e24726a9aecb9d3de05
Florian Bruggisser <github@broox.ch>
2024-02-16 10:39:42 +0100

    added bezier curve

--- prompt-latent-walk.py
+++ prompt-latent-walk.py
@@ -1,5 +1,6 @@
 from ImageGenerator import ImageGenerator
 from LatentPromptWalker import LatentPromptWalker
+from utils import curve_utils
 from utils.media_utils import generate_video_from_pil_images
 
 
@@ -13,7 +14,14 @@ def main():
     generator = ImageGenerator.from_pretrained("stabilityai/sdxl-turbo")
     walker = LatentPromptWalker(generator)
 
-    images = walker.generate_walk(prompts, 10, batch_size=16)
+    # create interpolation curve
+    curve = curve_utils.symmetric_cubic_bezier(0.25, 0.9)
+
+    def bezier_interpolate(x: float) -> float:
+        result = curve.evaluate(float(x))
+        return float(result[1][0])
+
+    images = walker.generate_walk(prompts, 10, batch_size=16, interpolation_method=bezier_interpolate)
     generate_video_from_pil_images("output/cat-dog-rabbit-walk.mp4", images, 10)
 
 
